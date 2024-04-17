3b51346ee174949894260481b02db2a978be9f89
mateusvrs <mateusvrs.dev@gmail.com>
2024-04-17 17:53:34 -0300

    code(templates): add basic c

---
+++ template/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void solve() {
+
+}
+
+int main() {
+    int t = 1;
+    //scanf("%d", &t);
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
